6ff746820802426e92389f099bf4480d21490684
Cmaggio3 <Cmaggio3@gatech.edu>
2019-04-18 09:47:02 -0400

    switching backing database

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-	host: "jacobsladderintaketeam.cik1yin3pif1.us-east-1.rds.amazonaws.com",
-	user: "intaketeam",
-	password: "IwantanA123",
-	database: "intaketeam",
+	host: "az1-ss18.a2hosting.com",
+	user: "jacobsla_Admin",
+	password: "JacobsLadderGT2019",
+	database: "jacobsla_jacobsLadder",
     debug    :  true
 });
 
