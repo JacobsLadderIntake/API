706936a5aa2fb99f101b7ef5d63c9881d853c3ee
Cmaggio3 <Cmaggio3@gatech.edu>
2019-04-22 19:22:28 -0400

    ahh

--- database.js
+++ database.js
@@ -1,5 +1,44 @@
 var mysql = require('mysql');
 
+
+
+
+var db_config = {
+	host: "az1-ss18.a2hosting.com",
+	user: "jacobsla_Admin",
+	password: "JacobsLadderGT2019",
+	database: "jacobsla_jacobsLadder",
+    debug    :  true
+}
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+module.exports = connection;
+
+/*
 var connection = mysql.createConnection({
 	host: "az1-ss18.a2hosting.com",
 	user: "jacobsla_Admin",
@@ -13,3 +52,4 @@ connection.connect(function(err) {
 });
 
 module.exports = connection;
+*/
