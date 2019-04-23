8a7045dd35a6a7d3671115fb3ec3efaa997d1e16
Cmaggio3 <Cmaggio3@gatech.edu>
2019-04-22 22:33:36 -0400

    ahh

--- database.js
+++ database.js
@@ -2,13 +2,21 @@ var mysql = require('mysql');
 
 
 
-
+/*
 var db_config = {
 	host: "az1-ss18.a2hosting.com",
 	user: "jacobsla_Admin",
 	password: "JacobsLadderGT2019",
 	database: "jacobsla_jacobsLadder",
     debug    :  true
+}
+*/
+var db_config = {
+	host:"jacobsladderintaketeam.cik1yin3pif1.us-east-1.rds.amazonaws.com",
+	user: "intaketeam",
+	password: "IwantanA123",
+	database: "jacobsla_jacobsLadder",
+    debug    :  true	
 }
 
 var connection;
@@ -20,14 +28,15 @@ function handleDisconnect() {
   connection.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      setTimeout(handleDisconnect, 3000); // We introduce a delay before attempting to reconnect,
     }                                     // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
   connection.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
+      console.log('hello\n\n\n')
+	  handleDisconnect();                         // lost due to either server restart, or a
     } else {                                      // connnection idle timeout (the wait_timeout
       throw err;                                  // server variable configures this)
     }
