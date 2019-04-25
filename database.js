14525f4663a10b92262f987acf96ee540bc2f950
Cmaggio3 <Cmaggio3@gatech.edu>
2019-04-25 13:25:34 -0400

    added support for pool connection to resolve PROTOCOL_CONNECTION_LOST issue

--- database.js
+++ database.js
@@ -19,10 +19,62 @@ var db_config = {
     debug    :  true	
 }
 
+
 var connection;
 
-function handleDisconnect() {
 
+
+
+function handleDisconnect() {
+  if(connection) connection.destroy()
+  connection = mysql.createPool(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused
+												  
+												  
+	console.log(db_config)
+												  
+												  
+  connection.getConnection(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 10000); // We introduce a delay before attempting to reconnect,
+    }
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+	  connection.destroy();
+	  setTimeout(handleDisconnect,10000);                  // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+  
+  	connection.query('select 1', function(err, results) {
+        if (err) console.log('SELECT', err.code);
+        else console.log('SELECT', results);
+    });
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function handleDisconnect() {
+  if(connection) connection.destroy()
   connection = mysql.createConnection(db_config); // Recreate the connection, since
                                                   // the old one cannot be reused
 												  
@@ -34,21 +86,36 @@ function handleDisconnect() {
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 10000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
+    }
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
   connection.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
 	  connection.destroy();
-	  handleDisconnect();                         // lost due to either server restart, or a
+	  setTimeout(handleDisconnect,10000);                  // lost due to either server restart, or a
     } else {                                      // connnection idle timeout (the wait_timeout
       throw err;                                  // server variable configures this)
     }
   });
+  
+  	connection.query('select 1', function(err, results) {
+        if (err) console.log('SELECT', err.code);
+        else console.log('SELECT', results);
+    });
+  
 }
+*/
 
 handleDisconnect();
+
+/*setInterval(function() {
+    connection.query('select 1', function(err, results) {
+        if (err) console.log('SELECT', err.code);
+        else console.log('SELECT', results);
+    });
+}, 60000);
+*/
 
 module.exports = connection;
 
