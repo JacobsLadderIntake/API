f2b86e3812eaa36ff8b1d6facce7286f791e4352
Cmaggio3 <Cmaggio3@gatech.edu>
2019-07-30 19:52:32 -0700

    corsOptions

--- server.js
+++ server.js
@@ -38,13 +38,12 @@ app.listen(port, function() {
 
 app.use(cors())
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
 });
-
-app.options('*', cors());
 
 app.post('/signup', addNewUser);
 app.post('/userlogin', userLoginCheck);
